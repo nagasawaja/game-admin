93eb013bbfffbb7d9db893e631926fe4a9575c21
huangliangxu <411030589@qq.com>
2019-11-21 10:28:21 +0800

    live2d

--- adminfront/src/App.vue
+++ adminfront/src/App.vue
@@ -1,21 +1,66 @@
 <template>
+
   <div id="app">
+
       <keep-alive>
           <router-view></router-view>
       </keep-alive>
+
   </div>
+
 </template>
 
 <script>
 
-export default{
-  name: 'APP',
+export default {
+    name: 'APP',
     data() {
-      return {
-          title: ''
-      }
+        return {
+            title: '',
+
+        }
     },
+
+
     created() {
-    },
+        setTimeout(() => {
+            var a = {
+                pluginRootPath: '/static/live2dw/',
+                pluginJsPath: '/static/live2dw/lib/',
+                pluginModelPath: '/static/live2dw/mashiro/',
+                tagMode: false,
+                debug: false,
+                model: {jsonPath: '/static/live2dw/mashiro/ryoufuku.model.json'},
+                display: {position: 'right', width: 450, height: 450},
+                mobile: {show: false},
+                log: true
+            };
+            var b = {
+                pluginRootPath: '/static/live2dw/',
+                pluginJsPath: '/static/live2dw/lib/',
+                pluginModelPath: '/static/live2dw/mashiro/',
+                tagMode: false,
+                debug: false,
+                model: {jsonPath: '/static/live2dw/mashiro/seifuku.model.json'},
+                display: {position: 'right', width: 450, height: 450},
+                mobile: {show: false},
+                log: true
+            };
+            var c = {
+                pluginRootPath: '/static/live2dw/',
+                pluginJsPath: '/static/live2dw/lib/',
+                pluginModelPath: '/static/live2dw/mashiro/',
+                tagMode: false,
+                debug: false,
+                model: {jsonPath: '/static/live2dw/mashiro/shifuku.model.json'},
+                display: {position: 'right', width: 450, height: 450},
+                mobile: {show: false},
+                log: true
+            };
+            var items = [a, b, c]
+            console.log(items[Math.floor(Math.random()*items.length)])
+            window.L2Dwidget.init(items[Math.floor(Math.random()*items.length)])
+        }, 1000)
+    }
 }
 </script>
