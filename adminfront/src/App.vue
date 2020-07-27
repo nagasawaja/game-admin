367937669f28b9cacbb92b38209da00f39370adc
huangliangxu <411030589@qq.com>
2020-07-27 15:17:27 +0800

    去除live2D

--- adminfront/src/App.vue
+++ adminfront/src/App.vue
@@ -22,52 +22,52 @@ export default {
     },
 
     mounted() {
-        setTimeout(()=> {
-            $('#live2d-widget').draggable()
-        },3000)
+        // setTimeout(()=> {
+        //     $('#live2d-widget').draggable()
+        // },3000)
     },
     created() {
-        setTimeout(() => {
-            var a = {
-                pluginRootPath: '/static/live2dw/',
-                pluginJsPath: '/static/live2dw/lib/',
-                pluginModelPath: '/static/live2dw/mashiro/',
-                tagMode: false,
-                debug: false,
-                model: {jsonPath: '/static/live2dw/mashiro/ryoufuku.model.json'},
-                display: {position: 'right', width: 450, height: 450},
-                mobile: {show: false},
-                log: true,
-                index:2,
-            };
-            var b = {
-                pluginRootPath: '/static/live2dw/',
-                pluginJsPath: '/static/live2dw/lib/',
-                pluginModelPath: '/static/live2dw/mashiro/',
-                tagMode: false,
-                debug: false,
-                model: {jsonPath: '/static/live2dw/mashiro/seifuku.model.json'},
-                display: {position: 'right', width: 450, height: 450},
-                mobile: {show: false},
-                log: true,
-                index:2,
-            };
-            var c = {
-                pluginRootPath: '/static/live2dw/',
-                pluginJsPath: '/static/live2dw/lib/',
-                pluginModelPath: '/static/live2dw/mashiro/',
-                tagMode: false,
-                debug: false,
-                model: {jsonPath: '/static/live2dw/mashiro/shifuku.model.json'},
-                display: {position: 'right', width: 450, height: 450},
-                mobile: {show: false},
-                log: true,
-                index:1
-            };
-            var items = [a, b, c]
-            console.log(items[Math.floor(Math.random()*items.length)])
-            window.L2Dwidget.init(items[Math.floor(Math.random()*items.length)])
-        }, 1000)
+        // setTimeout(() => {
+        //     var a = {
+        //         pluginRootPath: '/static/live2dw/',
+        //         pluginJsPath: '/static/live2dw/lib/',
+        //         pluginModelPath: '/static/live2dw/mashiro/',
+        //         tagMode: false,
+        //         debug: false,
+        //         model: {jsonPath: '/static/live2dw/mashiro/ryoufuku.model.json'},
+        //         display: {position: 'right', width: 450, height: 450},
+        //         mobile: {show: false},
+        //         log: true,
+        //         index:2,
+        //     };
+        //     var b = {
+        //         pluginRootPath: '/static/live2dw/',
+        //         pluginJsPath: '/static/live2dw/lib/',
+        //         pluginModelPath: '/static/live2dw/mashiro/',
+        //         tagMode: false,
+        //         debug: false,
+        //         model: {jsonPath: '/static/live2dw/mashiro/seifuku.model.json'},
+        //         display: {position: 'right', width: 450, height: 450},
+        //         mobile: {show: false},
+        //         log: true,
+        //         index:2,
+        //     };
+        //     var c = {
+        //         pluginRootPath: '/static/live2dw/',
+        //         pluginJsPath: '/static/live2dw/lib/',
+        //         pluginModelPath: '/static/live2dw/mashiro/',
+        //         tagMode: false,
+        //         debug: false,
+        //         model: {jsonPath: '/static/live2dw/mashiro/shifuku.model.json'},
+        //         display: {position: 'right', width: 450, height: 450},
+        //         mobile: {show: false},
+        //         log: true,
+        //         index:1
+        //     };
+        //     var items = [a, b, c]
+        //     console.log(items[Math.floor(Math.random()*items.length)])
+        //     window.L2Dwidget.init(items[Math.floor(Math.random()*items.length)])
+        // }, 1000)
     }
 }
 </script>
